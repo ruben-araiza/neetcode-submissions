4f8fa860c7d897531bb613633c6ada2a2dad5211
Ruben Dario Araiza Acosta <50540512+ruben-araiza@users.noreply.github.com>
2026-04-17 22:31:31 -0600

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        index = 0
+        res = []
+        while index < n:
+            current = nums.pop(index)
+            lookup = target - current
+            if lookup in nums:
+                nums.insert(index, -10000001)
+                res = [index, nums.index(lookup)]
+                break
+            nums.insert(index, current)
+            index += 1
+        return res
